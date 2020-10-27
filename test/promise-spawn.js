5a7c615eea11ba4c540210c0a29c843da5def060
isaacs <i@izs.me>
2020-10-27 11:07:00 -0700

    Do not return 0-length buffer if stdio is inherited

--- test/promise-spawn.js
+++ test/promise-spawn.js
@@ -4,15 +4,20 @@ const Minipass = require('minipass')
 const EE = require('events')
 const fs = require('fs')
 
+const isPipe = (stdio = 'pipe', fd) =>
+  stdio === 'pipe' || stdio === null ? true
+  : Array.isArray(stdio) ? isPipe(stdio[fd], fd)
+  : false
+
 class MockProc extends EE {
   constructor (cmd, args, opts) {
     super()
     this.cmd = cmd
     this.args = args
     this.opts = opts
-    this.stdout = opts.stdio === 'inherit' ? null : new Minipass()
-    this.stderr = opts.stdio === 'inherit' ? null : new Minipass()
-    this.stdin = opts.stdio === 'inherit' ? null : new Minipass()
+    this.stdin = isPipe(opts.stdio, 0) ? new Minipass() : null
+    this.stdout = isPipe(opts.stdio, 1) ? new Minipass() : null
+    this.stderr = isPipe(opts.stdio, 2) ? new Minipass() : null
     this.code = null
     this.signal = null
     process.nextTick(() => this.run())
@@ -111,8 +116,24 @@ t.test('pass', t => t.resolveMatch(promiseSpawn('pass', [], {stdioString: true},
 t.test('pass, share stdio', t => t.resolveMatch(promiseSpawn('pass', [], { stdio: 'inherit'}, {a: 1}), {
   code: 0,
   signal: null,
-  stdout: Buffer.alloc(0),
-  stderr: Buffer.alloc(0),
+  stdout: null,
+  stderr: null,
+  a: 1,
+}))
+
+t.test('pass, share stdout', t => t.resolveMatch(promiseSpawn('pass', [], { stdioString: true, stdio: ['pipe', 'inherit', 'pipe']}, {a: 1}), {
+  code: 0,
+  signal: null,
+  stdout: null,
+  stderr: '',
+  a: 1,
+}))
+
+t.test('pass, share stderr', t => t.resolveMatch(promiseSpawn('pass', [], { stdioString: true, stdio: ['pipe', 'pipe', 'inherit']}, {a: 1}), {
+  code: 0,
+  signal: null,
+  stdout: 'OK :)',
+  stderr: null,
   a: 1,
 }))
 
@@ -129,8 +150,8 @@ t.test('fail, shared stdio', t => t.rejects(promiseSpawn('fail', [], { stdio: 'i
   message: 'command failed',
   code: 1,
   signal: null,
-  stdout: Buffer.alloc(0),
-  stderr: Buffer.alloc(0),
+  stdout: null,
+  stderr: null,
   a: 1,
 }))
 
