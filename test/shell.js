fd6965ee86df80c4ee97768a032311e404d7b4f8
nlf <quitlahok@gmail.com>
2022-10-27 14:00:49 -0700

    feat: implement argument escaping when the `shell` option is set
    BREAKING CHANGE: when the `shell` option is set provided arguments will automatically be escaped

---
+++ test/shell.js
@@ -0,0 +1,210 @@
+'use strict'
+
+const spawk = require('spawk')
+const t = require('tap')
+
+const promiseSpawn = require('../lib/index.js')
+
+spawk.preventUnmatched()
+t.afterEach(() => {
+  spawk.clean()
+})
+
+t.test('sh', (t) => {
+  t.test('runs in shell', async (t) => {
+    const proc = spawk.spawn('sh', ['-c', 'echo hello'], { shell: false })
+      .stdout(Buffer.from('hello\n'))
+
+    const result = await promiseSpawn('echo', ['hello'], { shell: 'sh' })
+    t.hasStrict(result, {
+      code: 0,
+      signal: null,
+      stdout: Buffer.from('hello\n'),
+      stderr: Buffer.from(''),
+    })
+
+    t.ok(proc.called)
+  })
+
+  t.test('escapes arguments', async (t) => {
+    const proc = spawk.spawn('sh', ['-c', 'echo \'hello world\''], { shell: false })
+      .stdout(Buffer.from('hello\n'))
+
+    const result = await promiseSpawn('echo', ['hello world'], { shell: 'sh' })
+    t.hasStrict(result, {
+      code: 0,
+      signal: null,
+      stdout: Buffer.from('hello\n'),
+      stderr: Buffer.from(''),
+    })
+
+    t.ok(proc.called)
+  })
+
+  t.end()
+})
+
+t.test('cmd', (t) => {
+  t.test('runs in shell', async (t) => {
+    const proc = spawk.spawn('cmd.exe', ['/d', '/s', '/c', 'echo hello'], {
+      shell: false,
+      windowsVerbatimArguments: true,
+    })
+      .stdout(Buffer.from('hello\n'))
+
+    const result = await promiseSpawn('echo', ['hello'], { shell: 'cmd.exe' })
+    t.hasStrict(result, {
+      code: 0,
+      signal: null,
+      stdout: Buffer.from('hello\n'),
+      stderr: Buffer.from(''),
+    })
+
+    t.ok(proc.called)
+  })
+
+  t.test('works when initial cmd is wrapped in quotes', async (t) => {
+    const proc = spawk.spawn('cmd.exe', ['/d', '/s', '/c', '"echo" hello'], {
+      shell: false,
+      windowsVerbatimArguments: true,
+    })
+      .stdout(Buffer.from('hello\n'))
+
+    const result = await promiseSpawn('"echo"', ['hello'], { shell: 'cmd.exe' })
+    t.hasStrict(result, {
+      code: 0,
+      signal: null,
+      stdout: Buffer.from('hello\n'),
+      stderr: Buffer.from(''),
+    })
+
+    t.ok(proc.called)
+  })
+
+  t.test('works when initial cmd has a space and is wrapped in quotes', async (t) => {
+    const proc = spawk.spawn('cmd.exe', ['/d', '/s', '/c', '"two words" hello'], {
+      shell: false,
+      windowsVerbatimArguments: true,
+    })
+      .stdout(Buffer.from('hello\n'))
+
+    const result = await promiseSpawn('"two words"', ['hello'], { shell: 'cmd.exe' })
+    t.hasStrict(result, {
+      code: 0,
+      signal: null,
+      stdout: Buffer.from('hello\n'),
+      stderr: Buffer.from(''),
+    })
+
+    t.ok(proc.called)
+  })
+
+  t.test('works when initial cmd is more than one command', async (t) => {
+    const proc = spawk.spawn('cmd.exe', ['/d', '/s', '/c', 'one two three hello'], {
+      shell: false,
+      windowsVerbatimArguments: true,
+    })
+      .stdout(Buffer.from('hello\n'))
+
+    const result = await promiseSpawn('one two three', ['hello'], { shell: 'cmd.exe' })
+    t.hasStrict(result, {
+      code: 0,
+      signal: null,
+      stdout: Buffer.from('hello\n'),
+      stderr: Buffer.from(''),
+    })
+
+    t.ok(proc.called)
+  })
+
+  t.test('escapes when cmd is a .exe', async (t) => {
+    const promiseSpawnMock = t.mock('../lib/index.js', {
+      which: {
+        sync: (key, opts) => {
+          t.equal(key, 'dir')
+          return 'dir.exe'
+        },
+      },
+    })
+
+    const proc = spawk.spawn('cmd.exe', ['/d', '/s', '/c', 'dir ^"with^ spaces^"'], {
+      shell: false,
+      windowsVerbatimArguments: true,
+    })
+
+    const result = await promiseSpawnMock('dir', ['with spaces'], { shell: 'cmd.exe' })
+    t.hasStrict(result, {
+      code: 0,
+      signal: null,
+      stdout: Buffer.from(''),
+      stderr: Buffer.from(''),
+    })
+
+    t.ok(proc.called)
+  })
+
+  t.test('double escapes when cmd is a .cmd', async (t) => {
+    const promiseSpawnMock = t.mock('../lib/index.js', {
+      which: {
+        sync: (key, opts) => {
+          t.equal(key, 'dir')
+          return 'dir.cmd'
+        },
+      },
+    })
+
+    const proc = spawk.spawn('cmd.exe', ['/d', '/s', '/c', 'dir ^^^"with^^^ spaces^^^"'], {
+      shell: false,
+      windowsVerbatimArguments: true,
+    })
+
+    const result = await promiseSpawnMock('dir', ['with spaces'], { shell: 'cmd.exe' })
+    t.hasStrict(result, {
+      code: 0,
+      signal: null,
+      stdout: Buffer.from(''),
+      stderr: Buffer.from(''),
+    })
+
+    t.ok(proc.called)
+  })
+
+  t.test('which respects provided env PATH/PATHEXT', async (t) => {
+    const PATH = 'C:\\Windows\\System32'
+    const PATHEXT = 'EXE'
+
+    const promiseSpawnMock = t.mock('../lib/index.js', {
+      which: {
+        sync: (key, opts) => {
+          t.equal(key, 'dir')
+          t.equal(opts.path, PATH)
+          t.equal(opts.pathext, PATHEXT)
+          return 'dir.exe'
+        },
+      },
+    })
+
+    const proc = spawk.spawn('cmd.exe', ['/d', '/s', '/c', 'dir ^"with^ spaces^"'], {
+      shell: false,
+      windowsVerbatimArguments: true,
+    })
+
+    const result = await promiseSpawnMock('dir', ['with spaces'], {
+      env: {
+        PATH,
+        PATHEXT,
+      },
+      shell: 'cmd.exe',
+    })
+    t.hasStrict(result, {
+      code: 0,
+      signal: null,
+      stdout: Buffer.from(''),
+      stderr: Buffer.from(''),
+    })
+
+    t.ok(proc.called)
+  })
+
+  t.end()
+})
